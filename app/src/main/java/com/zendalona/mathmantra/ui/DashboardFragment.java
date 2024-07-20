7d02f4029b2a43d798e6019ad65e5e95feeda2b8
Ayush Prasad <104075394+ayush27prasad@users.noreply.github.com>
2024-07-20 14:46:58 +0530

    Added Dashboard Fragment

---
+++ app/src/main/java/com/zendalona/mathmantra/ui/DashboardFragment.java
@@ -0,0 +1,29 @@
+package com.zendalona.mathmantra.ui;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.zendalona.mathmantra.R;
+
+
+public class DashboardFragment extends Fragment {
+
+    public DashboardFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        // TODO : sara code yaha
+        return view;
+    }
+}
